7f3eb84ccfc8f509f7d9826d1e4f38a1ca1ba360
Duc Huynh <duchm@imt.com.vn>
2013-11-22 14:58:56 +0700

    summit project.

---
+++ TextEditor/TextEditor/MainView.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace TextEditor
+{
+	partial class MainView
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(182, 12);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(434, 461);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// listView1
+			// 
+			this.listView1.Location = new System.Drawing.Point(12, 12);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(164, 461);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(622, 41);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(135, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "FIND NEXT";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(622, 70);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(135, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "REPLACE";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(622, 450);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(135, 23);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "FINISH";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(622, 12);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(135, 23);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "OPEN";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(769, 485);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.richTextBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "CONFIG EDITOR";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+	}
+}
+
